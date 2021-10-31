97a49f85083e7e36749ef5f40bb3cb314f0245c0
Shuhail <shukad333@gmail.com>
2021-10-31 21:35:52 +0530

    detail page

---
+++ lib/ui/vehicle_detail_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class VehicleDetailsPage extends StatefulWidget {
+  const VehicleDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  _VehicleDetailsPageState createState() => _VehicleDetailsPageState();
+}
+
+class _VehicleDetailsPageState extends State<VehicleDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+
+
+    final topContent = Stack(
+      children: <Widget>[
+        Container(
+            padding: EdgeInsets.only(left: 10.0),
+            height: MediaQuery.of(context).size.height * 0.5,
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new AssetImage("drive-steering-wheel.jpg"),
+                fit: BoxFit.cover,
+              ),
+            )),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.5,
+          padding: EdgeInsets.all(40.0),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, .9)),
+          child: Center(
+            child: topContentText,
+          ),
+        ),
+        Positioned(
+          left: 8.0,
+          top: 60.0,
+          child: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(Icons.arrow_back, color: Colors.white),
+          ),
+        )
+      ],
+    );
+  }
+
+
+
+}
